9a44a1d77b35bd7dd552c4e0912a5cc987e18ade
thanhtuanmystic <tuanvp2001@gmail.com>
2024-10-07 15:29:01 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,7 @@ $(document).ready(function () {
   });
 });
 
-// Mảng chứa dữ liệu các gói "Diamond"
+// Mảng chứa dữ liệu các gói báo giá(trang chủ)
 const businessDiamondPackages = [
   { quantity: 300, price: "460.000" },
   { quantity: 500, price: "650.000" },
@@ -151,7 +151,96 @@ const htc78Packages = [
   { year: "2 năm", quantity: 600, price: "1.450.000" },
   { year: "3 năm", quantity: 1000, price: "2.100.000" },
 ];
-// Common function to generate package HTML
+
+// Mảng chứa dữ liệu các gói báo giá(trang chi tiết báo giá)
+const businessDiamondPackagesFinal = [
+  {
+    quantity: 300,
+    price: "1.560.000",
+    unitPrice: "460.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 500,
+    price: "1.750.000",
+    unitPrice: "650.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 1000,
+    price: "2.200.000",
+    unitPrice: "1.100.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 3000,
+    price: "3.400.000",
+    unitPrice: "2.300.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 5000,
+    price: "3.800.000",
+    unitPrice: "3.200.000đ/gói",
+    initFee: "Miễn phí",
+  },
+  {
+    quantity: 10000,
+    price: "6.300.000",
+    unitPrice: " 5.700.000đ/gói",
+    initFee: "Miễn phí",
+  },
+];
+const businessGoldPackagesFinal = [
+  {
+    quantity: 300,
+    price: "960.000",
+    unitPrice: "460.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 500,
+    price: "1.150.000",
+    unitPrice: "650.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 1000,
+    price: "1.600.000",
+    unitPrice: "1.100.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 3000,
+    price: "2.800.000",
+    unitPrice: "2.300.000đ/gói",
+    initFee: "500.000đ/gói",
+  },
+  {
+    quantity: 5000,
+    price: "3.200.000",
+    unitPrice: "3.200.000đ/gói",
+    initFee: "Miễn phí",
+  },
+  {
+    quantity: 10000,
+    price: "5.700.000",
+    unitPrice: " 5.700.000đ/gói",
+    initFee: "Miễn phí",
+  },
+];
+
+const htc88PackagesFinal = [
+  { year: "1 năm", quantity: 300, price: "1.400.000" },
+  { year: "2 năm", quantity: 600, price: "2.450.000" },
+  { year: "3 năm", quantity: 1000, price: "3.600.000" },
+];
+const htc78PackagesFinal = [
+  { year: "1 năm", quantity: 300, price: "800.000" },
+  { year: "2 năm", quantity: 600, price: "1.450.000" },
+  { year: "3 năm", quantity: 1000, price: "2.100.000" },
+];
+// Hàm sinh báo giá trang chủ
 function appendPackages(selector, packages, type) {
   $.each(packages, function (index, pkg) {
     const quantityOrYear = type === "year" ? pkg.year : pkg.quantity;
@@ -180,6 +269,117 @@ appendPackages("#business-diamond-list", businessDiamondPackages, "Diamond");
 appendPackages("#business-gold-list", businessGoldPackages, "Gold");
 appendPackages("#htc88-list", htc88Packages, "year");
 appendPackages("#htc78-list", htc78Packages, "year");
+
+// Hàm sinh báo giá chi tiết
+function appendPackagesFinal(selector, packages, type) {
+  $.each(packages, function (index, pkg) {
+    const quantityOrYear = type === "year" ? pkg.year : pkg.quantity;
+    const isGold = type === "Gold" ? true : false;
+    $(selector).append(`
+      <div class="price-box">
+        <div class="price-name">${
+          type === "year" ? "Gói" : type
+        } <span>${quantityOrYear}</span></div>
+        <div class="price-have">
+          <span>Tổng giá</span> 
+        </div>
+        <div class="price-money">
+          ${pkg.price} <span>vnđ/gói</span>
+        </div>
+        <a href="" class="ei-primary-btn">
+          <div>Mua ngay!</div>
+        </a>
+        <div class="price-detail">
+          <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> <span>${quantityOrYear}</span> hoá đơn điện tử
+          </div>
+          <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Đơn giá: <span>${
+              pkg.unitPrice
+            }</span>
+          </div>
+           <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Phí khởi tạo: <span>${
+              pkg.initFee
+            }</span>
+          </div>
+          ${
+            !isGold
+              ? `
+            <div class="price-have">
+             <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Phí hàng năm: <span>600.000đ/gói</span>
+           </div>`
+              : ""
+          }
+        </div>
+      </div>
+    `);
+  });
+}
+
+// Hàm sinh báo giá Hộ cá thể chi tiết
+function appendHCTPackagesFinal(selector, packages, type) {
+  const is88 = type === "88" ? true : false;
+  $.each(packages, function (index, pkg) {
+    $(selector).append(`
+      <div class="price-box">
+        <div class="price-name">Gói <span>${pkg.year}</span></div>
+        <div class="price-money">
+          ${pkg.price} <span>vnđ/gói</span>
+        </div>
+        <a href="" class="ei-primary-btn">
+          <div>Mua ngay!</div>
+        </a>
+        <div class="price-detail">
+          <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> <span>${
+              pkg.quantity
+            }</span> số hoá đơn điện tử
+          </div>
+          <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Chữ ký số
+          </div>
+            <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Mobile app
+          </div>
+          <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Hoá đơn đầu vào
+          </div>
+          ${
+            is88
+              ? `
+             <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Phần mềm kế toán
+          </div>
+          <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Tích hợp
+          </div>
+              `
+              : ""
+          }
+         
+          <div class="price-have">
+            <img srcset="./assets/img/tick-icon.png 2x" alt="Tick Icon" /> Web
+          </div>
+        </div>
+      </div>
+    `);
+  });
+}
+
+// Append packages to respective sections
+appendPackagesFinal(
+  "#business-diamond-list-final",
+  businessDiamondPackagesFinal,
+  "Diamond"
+);
+appendPackagesFinal(
+  "#business-gold-list-final",
+  businessGoldPackagesFinal,
+  "Gold"
+);
+appendHCTPackagesFinal("#htc88-list-final", htc88PackagesFinal, "88");
+appendHCTPackagesFinal("#htc78-list-final", htc78PackagesFinal, "78");
 
 // Scroll partner
 const scrollers = document.querySelectorAll(".scroller");
@@ -239,4 +439,36 @@ $(document).ready(function () {
       }, 100); // Delay để kiểm tra nếu con trỏ chuột không nằm trên div1
     }
   );
+});
+
+// Process Easyinvoice
+$(document).ready(function () {
+  const $faqItems = $(".faq-item");
+
+  $faqItems.on("click", function () {
+    const $item = $(this);
+    const $answer = $item.next();
+    const $icon = $item.find("i");
+    const $faqBox = $item.closest(".faq-box");
+    $(".faq-box").removeClass("faqBoxActive");
+    $faqItems.not($item).each(function () {
+      const $otherAnswer = $(this).next();
+      const $otherIcon = $(this).find("i");
+      $otherAnswer
+        .removeClass("active")
+        .css("max-height", "0")
+        .css("margin-top", 0);
+      $otherIcon.removeClass("active");
+    });
+
+    $answer.toggleClass("active");
+    $icon.toggleClass("active");
+    $answer
+      .css(
+        "max-height",
+        $answer.hasClass("active") ? $answer[0].scrollHeight + "px" : "0"
+      )
+      .css("margin-top", $answer.hasClass("active") ? "1rem" : "0");
+    $faqBox.addClass($answer.hasClass("active") ? "faqBoxActive" : "");
+  });
 });
